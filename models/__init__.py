0829d6d7e6607abafe90f023b06c6b58fd5abebd
albert-jin <248390697@qq.com>
2022-05-20 12:32:21 +0800

    first commit

---
+++ models/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# file: __init__.py
+# author: songyouwei <youwei0314@gmail.com>
+# Copyright (C) 2018. All Rights Reserved.
+
+from models.lstm import LSTM
+from models.ian import IAN
+from models.memnet import MemNet
+from models.ram import RAM
+from models.td_lstm import TD_LSTM
+from models.tc_lstm import TC_LSTM
+from models.cabasc import Cabasc
+from models.atae_lstm import ATAE_LSTM
+from models.tnet_lf import TNet_LF
+from models.aoa import AOA
+from models.mgan import MGAN
+from models.lcf_bert import LCF_BERT
+from models.asgcn import ASGCN
